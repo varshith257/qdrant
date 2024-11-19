ebfb98ed677d9c1a0c360ce661a384a0a7e8109f
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-11-19 11:23:15 +0530

    fix timeout issues

--- lib/collection/src/recommendations.rs
+++ lib/collection/src/recommendations.rs
@@ -23,8 +23,8 @@ use crate::common::fetch_vectors::{
     convert_to_vectors, convert_to_vectors_owned, resolve_referenced_vectors_batch,
     ReferencedVectors,
 };
-use crate::operations::consistency_params::ReadConsistency;
 use crate::common::retrieve_request_trait::RetrieveRequest;
+use crate::operations::consistency_params::ReadConsistency;
 use crate::operations::query_enum::QueryEnum;
 use crate::operations::shard_selector_internal::ShardSelectorInternal;
 use crate::operations::types::{
